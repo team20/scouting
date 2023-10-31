ee923d9b635d5981015097b368085245561f275f
Gold856 <117957790+Gold856@users.noreply.github.com>
2023-10-30 22:41:42 -0400

    Finish up match info screen

--- src/main-element.ts
+++ src/main-element.ts
@@ -1,18 +1,28 @@
 import { css, html, LitElement } from "lit";
 import { customElement } from "lit/decorators.js";
-import { createRef, Ref, ref } from "lit/directives/ref.js";
+import { createRef, Ref } from "lit/directives/ref.js";
 /**
  * An example element.
  *
  * @slot - This element has a slot
  * @csspart button - The button
  */
-@customElement("my-element")
-export class MyElement extends LitElement {
+@customElement("match-info")
+export class MatchInfo extends LitElement {
 	static styles = css`
 		:host {
 			display: flex;
 			flex-direction: column;
+			width: 25%;
+		}
+		label {
+			display: flex;
+			justify-content: space-between;
+			color: var(--lumo-secondary-text-color);
+			font-family: var(--lumo-font-family);
+			font-size: var(--lumo-font-size-m);
+			font-weight: 500;
+			line-height: 2;
 		}
 	`;
 	// @ts-ignore
@@ -22,24 +32,68 @@ export class MyElement extends LitElement {
 		{ label: "Cube", value: "Cube" },
 		{ label: "Cone", value: "Cone" },
 	];
+	matchTypes = [
+		{ label: "Practice", value: "Practice" },
+		{ label: "Qualification", value: "Qualification" },
+		{ label: "Playoffs", value: "Playoffs" },
+	];
+	alliances = [
+		{ label: "Blue", value: "Blue" },
+		{ label: "Red", value: "Red" },
+	];
+	startingPositions = [
+		{ label: "1", value: "1" },
+		{ label: "2", value: "2" },
+		{ label: "3", value: "3" },
+	];
 
 	render() {
-		return html`<vaadin-text-field
-				${ref(this.ref)}
-				label="Name"
-			></vaadin-text-field>
-			<vaadin-select
-				.items="${this.preloadOptions}"
-				label="Preloaded Piece?"
-			></vaadin-select>`;
-	}
-	test() {
-		console.log(this.ref.value!.value);
+		return html` <label
+				>Name:
+				<vaadin-text-field theme="small"></vaadin-text-field>
+			</label>
+			<label>
+				Match Type:
+				<vaadin-select
+					theme="small"
+					.items="${this.matchTypes}"
+				></vaadin-select>
+			</label>
+			<label>
+				Match Number:
+				<vaadin-integer-field theme="small"></vaadin-integer-field>
+			</label>
+			<label>
+				Replay?
+				<vaadin-checkbox></vaadin-checkbox>
+			</label>
+			<label>
+				Alliance:
+				<vaadin-select theme="small" .items="${this.alliances}"></vaadin-select>
+			</label>
+			<label>
+				Starting Position:
+				<vaadin-select
+					theme="small"
+					.items="${this.startingPositions}"
+				></vaadin-select>
+			</label>
+			<label>
+				Team Number:
+				<vaadin-integer-field theme="small"></vaadin-integer-field>
+			</label>
+			<label>
+				Preload:
+				<vaadin-select
+					theme="small"
+					.items="${this.preloadOptions}"
+				></vaadin-select>
+			</label>`;
 	}
 }
 
 declare global {
 	interface HTMLElementTagNameMap {
-		"my-element": MyElement;
+		"match-info": MatchInfo;
 	}
 }

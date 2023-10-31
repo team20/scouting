edfb15259d08c4c25446b9e46ebd06626b28063f
Gold856 <117957790+Gold856@users.noreply.github.com>
2023-10-30 23:45:23 -0400

    Add charge station info

---
+++ src/charge-station-info.ts
@@ -0,0 +1,49 @@
+import { css, html, LitElement } from "lit";
+import { customElement } from "lit/decorators.js";
+@customElement("charge-station-info")
+export class ChargeStationInfo extends LitElement {
+	static styles = css`
+		:host {
+			display: block;
+			text-align: center;
+			margin: 0;
+			width: min-content;
+		}
+		.checkbox {
+			color: var(--lumo-secondary-text-color);
+			font-family: var(--lumo-font-family);
+			font-size: var(--lumo-font-size-m);
+			font-weight: 500;
+			line-height: 2;
+		}
+	`;
+
+	/** Whether or not the team attempted to engage the charge station.*/
+	attemptedEngage = [
+		{ label: "None", value: "None" },
+		{ label: "Engaged", value: "Engaged" },
+	];
+	/** What the team's actual endgame was.*/
+	actualEndgame = [
+		{ label: "None", value: "None" },
+		{ label: "Park", value: "Park" },
+		{ label: "Dock", value: "Dock" },
+		{ label: "Engage", value: "Engage" },
+	];
+	render() {
+		return html` <vaadin-select
+				label="Attempted"
+				.items="${this.attemptedEngage}"
+			></vaadin-select>
+			<vaadin-select
+				label="Actual"
+				.items="${this.actualEndgame}"
+			></vaadin-select>`;
+	}
+}
+
+declare global {
+	interface HTMLElementTagNameMap {
+		"charge-station-info": ChargeStationInfo;
+	}
+}

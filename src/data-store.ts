7329f40828f1b818e07672be15f6d1ec64181c79
Gold856 <117957790+Gold856@users.noreply.github.com>
2023-10-31 08:16:37 -0400

    Make combineData return all scouting data

--- src/data-store.ts
+++ src/data-store.ts
@@ -4,21 +4,55 @@ import { GamePieceGrid } from "./grid";
 import { MatchInfo } from "./main-element";
 import { TeleopInfo } from "./teleop-info";
 
-export function combineData() {
-	let matchInfo = (document.getElementById("matchInfo") as MatchInfo).getInfo();
-	let autoScoring = (
-		document.getElementById("autoScoringInfo") as GamePieceGrid
-	).getScoringInfo();
-	let autoInfo = (document.getElementById("autoInfo") as AutoInfo).getInfo();
-	let teleopScoring = (
-		document.getElementById("teleopScoringInfo") as GamePieceGrid
-	).getScoringInfo();
-	let teleopInfo = (
-		document.getElementById("teleopInfo") as TeleopInfo
-	).getInfo();
-	let endInfo = (document.getElementById("endInfo") as EndScreen).getInfo();
-	console.log(matchInfo);
-	console.log(autoInfo);
-	console.log(teleopInfo);
-	console.log(endInfo);
+let matchScreen = document.getElementById("matchInfo")! as MatchInfo;
+let autoGrid = document.getElementById("autoScoringInfo")! as GamePieceGrid;
+let autoInfoBar = document.getElementById("autoInfo")! as AutoInfo;
+let teleopGrid = document.getElementById("teleopScoringInfo")! as GamePieceGrid;
+let teleopInfoBar = document.getElementById("teleopInfo")! as TeleopInfo;
+let endScreen = document.getElementById("endInfo")! as EndScreen;
+export function combineData(): string {
+	let matchInfo = matchScreen.getInfo();
+	let autoScoring = autoGrid.getScoringInfo();
+	let autoInfo = autoInfoBar.getInfo();
+	let teleopScoring = teleopGrid.getScoringInfo();
+	let teleopInfo = teleopInfoBar.getInfo();
+	let endInfo = endScreen.getInfo();
+	let autoGridList = "";
+	for (let i = 0; i < autoScoring.high.length; i++) {
+		if (autoScoring.high[i] === "None") {
+			autoGridList += `true\t`;
+		} else {
+			autoGridList += `false\t`;
+		}
+	}
+	for (let i = 0; i < autoScoring.middle.length; i++) {
+		if (autoScoring.middle[i] === "None") {
+			autoGridList += `true\t`;
+		} else {
+			autoGridList += `false\t`;
+		}
+	}
+	for (let i = 0; i < autoScoring.low.length; i++) {
+		autoGridList += `${autoScoring.low[i]}\t`;
+	}
+	let teleopGridList = "";
+	for (let i = 0; i < teleopScoring.high.length; i++) {
+		if (teleopScoring.high[i] === "None") {
+			teleopGridList += `true\t`;
+		} else {
+			teleopGridList += `false\t`;
+		}
+	}
+	for (let i = 0; i < teleopScoring.middle.length; i++) {
+		if (teleopScoring.middle[i] === "None") {
+			teleopGridList += `true\t`;
+		} else {
+			teleopGridList += `false\t`;
+		}
+	}
+	for (let i = 0; i < teleopScoring.low.length; i++) {
+		teleopGridList += `${teleopScoring.low[i]}\t`;
+	}
+	return `${matchInfo.name}	${matchInfo.matchType}	${matchInfo.matchNum}	${matchInfo.isReplay}	${matchInfo.alliance}	${matchInfo.startingPosition}	${matchInfo.teamNum}	${matchInfo.preload}	${autoGridList}${autoInfo.conesDropped}	${autoInfo.cubesDropped}	${autoInfo.mobility}	${autoInfo.attemptedEndgame}	${autoInfo.actualEndgame}	${teleopGridList}${teleopInfo.conesDropped}	${teleopInfo.cubesDropped}	${teleopInfo.fouls}	${teleopInfo.techFouls}	${teleopInfo.attemptedEndgame}	${teleopInfo.actualEndgame}	${endInfo.defenseQualityFaced}	${endInfo.defenseQuantityFaced}	${endInfo.defenseQualityPlayed}	${endInfo.defenseQuantityPlayed}	${endInfo.breakdown}	${endInfo.chargeStationClimbTime}	${endInfo.chargeStationSide}	comments: ${endInfo.comments}	`;
 }
+export function resetSession() {}

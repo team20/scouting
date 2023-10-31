3d264ff30d9bb541753a3a039a2cf5d0b4df51bc
Gold856 <117957790+Gold856@users.noreply.github.com>
2023-10-31 00:15:28 -0400

    Toggle Button

---
+++ src/toggle-button.ts
@@ -0,0 +1,63 @@
+import { css, html, LitElement } from "lit";
+import { customElement, property } from "lit/decorators.js";
+@customElement("toggle-button")
+export class ToggleButton extends LitElement {
+	static styles = css`
+		:host {
+			display: block;
+			max-width: 1280px;
+			text-align: center;
+			margin: 0;
+			width: min-content;
+		}
+		vaadin-button {
+			user-select: none;
+			line-height: 100px;
+			color: #000000;
+			display: block;
+			height: min-content;
+			cursor: pointer;
+			margin: 0;
+		}
+		.off {
+			--lumo-contrast-5pct: #cccccc;
+		}
+		.on {
+			--lumo-contrast-5pct: #019d04;
+		}
+	`;
+	@property()
+	label: string;
+
+	@property()
+	toggled: boolean = false;
+
+	render() {
+		return html`<vaadin-button
+			class=${this.calculateColor()}
+			@click=${this.onClick}
+			>${this.label}</vaadin-button
+		>`;
+	}
+
+	onClick() {
+		this.toggled = !this.toggled;
+	}
+
+	/**
+	 *
+	 * @returns The class name for the color
+	 */
+	calculateColor() {
+		if (this.toggled) {
+			return "on";
+		}
+		return "off";
+	}
+}
+
+declare global {
+	interface HTMLElementTagNameMap {
+		"toggle-button": ToggleButton;
+	}
+}

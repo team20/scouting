086d1ed105b691e6aca39f5cd1211b1ba5572607
Gold856 <117957790+Gold856@users.noreply.github.com>
2023-11-03 07:35:59 -0400

    Increment version number

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Increment when you update the repo
-const cacheName = "Team20-Scouting-v4";
+const cacheName = "Team20-Scouting-v5";
 // URLs for all the assets
 const resourceFiles = [];
 self.addEventListener("install", (e) => {

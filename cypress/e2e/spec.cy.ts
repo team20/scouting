d36cbe8e9fe7adfc83c68f78e988ee1fcc5698a8
Gold856 <117957790+Gold856@users.noreply.github.com>
2023-11-05 23:35:15 -0500

    Add tests

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,185 @@
+/// <reference types="cypress" />
+
+describe("Scouting data validation", () => {
+	it("passes", () => {
+		cy.visit("/scouting");
+		// Match Info tab
+		cy.get("#matchInfo")
+			.shadow()
+			.find("div > label:nth-child(1) > vaadin-text-field")
+			.type("Scouter Name");
+		cy.get("#matchInfo")
+			.shadow()
+			.find("div > label:nth-child(2) > vaadin-select")
+			.click();
+		cy.get(
+			"body > vaadin-select-overlay > vaadin-select-list-box > vaadin-select-item:nth-child(2)"
+		).click();
+		cy.root().click(); // Required before typing into another input field to fix weird focus bug
+		cy.get("#matchInfo")
+			.shadow()
+			.find("div > label:nth-child(3) > vaadin-integer-field")
+			.type("1");
+		cy.get("#matchInfo")
+			.shadow()
+			.find("div > label:nth-child(5) > vaadin-select")
+			.click();
+		cy.get(
+			"body > vaadin-select-overlay > vaadin-select-list-box > vaadin-select-item:nth-child(2)"
+		).click();
+		cy.get("#matchInfo")
+			.shadow()
+			.find("div > label:nth-child(6) > vaadin-select")
+			.click();
+		cy.get(
+			"body > vaadin-select-overlay > vaadin-select-list-box > vaadin-select-item:nth-child(2)"
+		).click();
+		cy.root().click(); // Required before typing into another input field to fix weird focus bug
+		cy.get("#matchInfo")
+			.shadow()
+			.find("div > label:nth-child(7) > vaadin-integer-field")
+			.type("20");
+		cy.get("#matchInfo")
+			.shadow()
+			.find("div > label:nth-child(8) > vaadin-select")
+			.click();
+		cy.get(
+			"body > vaadin-select-overlay > vaadin-select-list-box > vaadin-select-item:nth-child(2)"
+		).click();
+
+		// Auto tab
+		cy.get("#auto-tab").click();
+		// Click the entire top row
+		for (let i = 1; i <= 9; i++) {
+			cy.get("#autoScoringInfo")
+				.shadow()
+				.find(`div:nth-child(1) > node-button:nth-child(${i})`)
+				.click();
+		}
+		// Skip the middle row, same as top row
+		// Click some of the bottom row
+		for (let i = 1; i <= 9; i++) {
+			// Don't click every third button
+			if (i % 3 == 0) {
+				// no-op
+			} else if (i % 3 == 1) {
+				// Click every first button once
+				cy.get("#autoScoringInfo")
+					.shadow()
+					.find(`div:nth-child(3) > node-button:nth-child(${i})`)
+					.click();
+			} else {
+				// Click every second button twice
+				cy.get("#autoScoringInfo")
+					.shadow()
+					.find(`div:nth-child(3) > node-button:nth-child(${i})`)
+					.click()
+					.click();
+			}
+		}
+		cy.get("#autoInfo")
+			.shadow()
+			.find("game-counter:nth-child(1)")
+			.shadow()
+			.find("vaadin-button.rightButton")
+			.click();
+		cy.get("#autoInfo")
+			.shadow()
+			.find("game-counter:nth-child(2)")
+			.shadow()
+			.find("vaadin-button.rightButton")
+			.click()
+			.click()
+			.click();
+		cy.get("#autoInfo")
+			.shadow()
+			.find("game-counter:nth-child(2)")
+			.shadow()
+			.find("vaadin-button.leftButton")
+			.click();
+		cy.get("#autoInfo").shadow().find("toggle-button").click();
+		cy.get("#autoInfo")
+			.shadow()
+			.find("charge-station-info")
+			.shadow()
+			.find("vaadin-select:nth-child(2)")
+			.click();
+		cy.get(
+			"body > vaadin-select-overlay > vaadin-select-list-box > vaadin-select-item:nth-child(4)"
+		).click();
+		cy.get("#autoInfo")
+			.shadow()
+			.find("charge-station-info")
+			.shadow()
+			.find("vaadin-select:nth-child(1)")
+			.click();
+		cy.get("body > vaadin-select-overlay").should("not.exist");
+
+		// Teleop tab
+		cy.get("#teleop-tab").click();
+		cy.get("#teleopInfo").shadow();
+		cy.get("#teleopInfo")
+			.shadow()
+			.find("game-counter:nth-child(1)")
+			.shadow()
+			.find("vaadin-button.rightButton")
+			.click();
+		cy.get("#teleopInfo")
+			.shadow()
+			.find("game-counter:nth-child(2)")
+			.shadow()
+			.find("vaadin-button.rightButton")
+			.click()
+			.click();
+		cy.get("#teleopInfo")
+			.shadow()
+			.find("game-counter:nth-child(3)")
+			.shadow()
+			.find("vaadin-button.rightButton")
+			.click()
+			.click()
+			.click();
+		cy.get("#teleopInfo")
+			.shadow()
+			.find("game-counter:nth-child(4)")
+			.shadow()
+			.find("vaadin-button.rightButton")
+			.click()
+			.click()
+			.click()
+			.click();
+		cy.get("#teleopInfo")
+			.shadow()
+			.find("charge-station-info")
+			.shadow()
+			.find("vaadin-select:nth-child(2)")
+			.click();
+		cy.get(
+			"body > vaadin-select-overlay > vaadin-select-list-box > vaadin-select-item:nth-child(3)"
+		).click();
+		cy.get("#teleopInfo")
+			.shadow()
+			.find("charge-station-info")
+			.shadow()
+			.find("vaadin-select:nth-child(1)")
+			.click();
+		cy.get(
+			"body > vaadin-select-overlay > vaadin-select-list-box > vaadin-select-item:nth-child(2)"
+		).click();
+		cy.get("#end-tab").click();
+		cy.root().click(); // Required before typing into another input field to fix weird focus bug
+		cy.get("#endInfo")
+			.shadow()
+			.find("div.info > vaadin-text-field")
+			.type("Tippy, struggled to move");
+		cy.get("#endInfo")
+			.shadow()
+			.find("div.diagramDiv > vaadin-button:nth-child(4)")
+			.click();
+		cy.fixture("scoutingData.txt").then((referenceData) =>
+			cy
+				.readFile("cypress/downloads/Red2QualificationScoutingData1.txt")
+				.should("eq", referenceData)
+		);
+	});
+});

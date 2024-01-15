708097a1b624f60003b6a26115a4195abe47eaf1
Gabriel West <gabedwest@gmail.com>
2024-01-14 20:52:32 -0500

    working on end screen

---
+++ src/breakdown-button.ts
@@ -0,0 +1,77 @@
+import { css, html, LitElement } from "lit";
+import { customElement, property } from "lit/decorators.js";
+/**
+ * A button that switches between red and Team 20 green.
+ */
+@customElement("breakdown-button")
+export class BreakdownButton extends LitElement {
+	static styles = css`
+		:host {
+			display: block;
+			text-align: center;
+			width: min-content;
+			height: 100%;
+			aspect-ratio: 1 / 1;
+		}
+		vaadin-button {
+			display: block;
+			height: 100%;
+			margin: 0;
+			line-height: 100px;
+			color: #000000;
+			/* color: var(--lumo-primary-text-color); */
+			cursor: pointer;
+		}
+
+
+
+		.off {
+			--lumo-contrast-5pct: #ff0000;
+		}
+		.on {
+			--lumo-contrast-5pct: #019d04;
+		}
+	`;
+	@property()
+	label!: string;
+	statusLabel: string = "No";
+	@property()
+	toggled: boolean = false;
+
+	render() {
+		return html`<vaadin-button
+			class=${this.calculateColor()}
+			@click=${this.onClick}
+			><h1 style="margin-bottom: 0;">${this.label}</h1><br><h3>${this.statusLabel}</h3></vaadin-button
+		>`;
+	}
+
+	onClick() {
+		this.toggled = !this.toggled;
+		this.statusLabel! = this.calculateLabel();
+	}
+
+	/**
+	 * Calculates the class name for the button color when the button is clicked.
+	 * @returns The class name for the color
+	 */
+	calculateColor() {
+		if (this.toggled) {
+			return "on";
+		}
+		return "off";
+	}
+
+	calculateLabel() {
+		if (this.toggled) {
+			return "Yes";
+		}
+		return "No";
+	}
+}
+
+declare global {
+	interface HTMLElementTagNameMap {
+		"toggle-button": ToggleButton;
+	}
+}
--- src/end-screen.ts
+++ src/end-screen.ts
@@ -1,13 +1,10 @@
-import { css, html, LitElement } from "lit";
-import { customElement } from "lit/decorators.js";
-import { createRef, ref, Ref } from "lit/directives/ref.js";
-import { toCanvas } from "qrcode";
-import {
-	combineData,
-	getMatchInfo,
-	resetSession,
-	storeData,
-} from "./data-store";
+import {css, html, LitElement} from "lit";
+import {customElement} from "lit/decorators.js";
+import {createRef, ref, Ref} from "lit/directives/ref.js";
+import {BreakdownButton} from "./breakdown-button.ts"
+import "@vaadin/text-area";
+
+
 /**
  * The final scouting screen.
  *
@@ -16,33 +13,8 @@ import {
  */
 @customElement("end-screen")
 export class EndScreen extends LitElement {
-	static styles = css`
-		:host {
-			display: grid;
-			grid-template-columns: min-content auto auto;
-			grid-column-gap: 10px;
-			text-align: left;
-		}
-		.info {
-			display: flex;
-			width: 400px;
-			flex-direction: column;
-			text-align: left;
-			margin: 0;
-		}
-		.diagramDiv {
-			display: flex;
-			flex-direction: column;
-			text-align: left;
-			margin: 0;
-			width: min-content;
-		}
-		vaadin-select,
-		vaadin-checkbox,
-		vaadin-integer-field,
-		vaadin-text-field {
-			padding-top: 0;
-		}
+    static styles = css`
+
 		label {
 			color: var(--lumo-secondary-text-color);
 			font-family: var(--lumo-font-family);
@@ -56,117 +28,169 @@ export class EndScreen extends LitElement {
 		}
 
 		#left {
-			width: 60%;
+			width: 120%;
+			height: 100%;
 		}
 
 		#right {
-			width: 40%;
-	background-color: red;
+			width: 100%;
+
+		}
+
+		#container {
+			display: flex;
+			width: 100%;
+			height: 100%;
+		}
+		#row1 {
+			display: flex;
+			align-items: center;
+  			justify-content: center;
+            gap: 10px;
+            height: 150px;
+		}
+		#row2 {
+			display: flex;
+            gap: 10px;
+            height: 150px;
+		}
+	#row3 {
+			display: flex;
+            height: 150px;
 		}
 	`;
 
-	defenseQualityOptions = [
-		{ label: "None", value: "None" },
-		{ label: "Attempted", value: "Attempted" },
-		{ label: "Good", value: "Good" },
-		{ label: "Excellent", value: "Excellent" },
-	];
-	defenseQuantityOptions = [
-		{ label: "None", value: "None" },
-		{ label: "Opportunistic", value: "Opportunistic" },
-		{ label: "Dedicated", value: "Dedicated" },
-	];
-	chargeStationSides = [
-		{ label: "1", value: "1" },
-		{ label: "2", value: "2" },
-		{ label: "3", value: "3" },
-	];
-	defenseQualityPlayed: Ref<HTMLInputElement> = createRef();
-	defenseQuantityPlayed: Ref<HTMLInputElement> = createRef();
-	defenseQualityFaced: Ref<HTMLInputElement> = createRef();
-	defenseQuantityFaced: Ref<HTMLInputElement> = createRef();
-	breakdown: Ref<HTMLInputElement> = createRef();
-	chargeStationClimbTime: Ref<HTMLInputElement> = createRef();
-	chargeStationSide: Ref<HTMLInputElement> = createRef();
-	comments: Ref<HTMLInputElement> = createRef();
-	sessionRestart: Ref<HTMLButtonElement> = createRef();
-	continueScouting: Ref<HTMLInputElement> = createRef();
-	canvas: Ref<HTMLCanvasElement> = createRef();
+    yesNoOptions = [
+        {label: "Yes", value: "Yes"},
+        {label: "No", value: "No"},
+    ];
 
-	render() {
-		return html`
-		<div id="left">
-		left
-		</div>
+    trapResultOptions = [
+        {label: "0", value: "0"},
+        {label: "1", value: "1"},
+        {label: "2", value: "2"},
+        {label: "3", value: "3"},
+    ];
 
-		<div id="right">
-		right
-		</div>
-		`;
-	}
-	/**
-	 * Combines all the data into JSON.
-	 * @returns An object containing this element's data
-	 */
-	getInfo() {
-		return {
-			defenseQualityPlayed: this.defenseQualityPlayed.value!.value,
-			defenseQuantityPlayed: this.defenseQuantityPlayed.value!.value,
-			defenseQualityFaced: this.defenseQualityFaced.value!.value,
-			defenseQuantityFaced: this.defenseQuantityFaced.value!.value,
-			breakdown: this.breakdown.value!.checked,
-			chargeStationClimbTime: this.chargeStationClimbTime.value!.value,
-			chargeStationSide: this.chargeStationSide.value!.value,
-			comments: this.comments.value!.value,
-		};
-	}
-	/**
-	 * Prepares this element for a new scouting session.
-	 *
-	 * Resets all values to their defaults.
-	 */
-	reset() {
-		this.defenseQualityPlayed.value!.value = "";
-		this.defenseQuantityPlayed.value!.value = "";
-		this.defenseQualityFaced.value!.value = "";
-		this.defenseQuantityFaced.value!.value = "";
-		this.breakdown.value!.checked = false;
-		this.chargeStationClimbTime.value!.value = "";
-		this.chargeStationSide.value!.value = "";
-		this.comments.value!.value = "";
-	}
-	restartSession() {
-		resetSession(this.continueScouting.value!.checked);
-		this.sessionRestart.value!.disabled = true;
-	}
-	renderQRCode() {
-		let data = combineData();
-		console.log(data);
-		toCanvas(
-			this.canvas.value,
-			data,
-			{ errorCorrectionLevel: "low" },
-			function (error) {
-				if (error) console.error(error);
-				console.log("success!");
-			}
-		);
-		let matchInfo = getMatchInfo();
-		let key = `${matchInfo.alliance}${matchInfo.startingPosition}${
-			matchInfo.matchType
-		}${matchInfo.isReplay ? "replay" : ""}ScoutingData${matchInfo.matchNum}`;
-		storeData(data, key);
-		let file = new Blob([data], { type: "text/plain" });
-		let link = document.createElement("a");
-		link.download = `${key}.txt`;
-		link.href = URL.createObjectURL(file);
-		link.click();
-		this.sessionRestart.value!.disabled = false;
-	}
+    harmonyOptions = [
+        {label: "0", value: "0"},
+        {label: "2", value: "2"},
+        {label: "3", value: "3"},
+    ];
+
+
+    trapAttempted: Ref<HTMLInputElement> = createRef();
+    trapResult: Ref<HTMLInputElement> = createRef();
+    climbAttempted: Ref<HTMLInputElement> = createRef();
+    climbResult: Ref<HTMLInputElement> = createRef();
+    harmony: Ref<HTMLInputElement> = createRef();
+    park: Ref<HTMLInputElement> = createRef();
+    breakdown: Ref<BreakdownButton> = createRef();
+    comments: Ref<HTMLInputElement> = createRef();
+
+    //TODO Will be back
+    //sessionRestart: Ref<HTMLButtonElement> = createRef();
+    //continueScouting: Ref<HTMLInputElement> = createRef();
+
+    render() {
+        return html`
+            <div id="container">
+                <div id="left">
+                    <div id="row1">
+
+                        <vaadin-select
+                            ${ref(this.trapAttempted)}
+                            theme="small"
+                            label="Trap Attempted?"
+                            .items="${this.yesNoOptions}"
+                        ></vaadin-select>
+
+                        <vaadin-select
+                            ${ref(this.trapResult)}
+                            theme="small"
+                            label="Traps Result"
+                            .items="${this.trapResultOptions}"
+                        ></vaadin-select>
+                    </div>
+                    <div id="row2">
+
+                        <vaadin-select
+                            ${ref(this.climbAttempted)}
+                            theme="small"
+                            label="Climb Attempted?"
+                            .items="${this.yesNoOptions}"
+                        ></vaadin-select>
+
+                        <vaadin-select
+                            ${ref(this.climbResult)}
+                            theme="small"
+                            label="Climb Result"
+                            .items="${this.yesNoOptions}"
+                        ></vaadin-select>
+
+
+                        <vaadin-select
+                            ${ref(this.harmony)}
+                            theme="small"
+                            label="Harmony?"
+                            .items="${this.harmonyOptions}"
+                        ></vaadin-select>
+
+
+                    </div>
+                    <div id="row3">
+                        <vaadin-select
+                            ${ref(this.park)}
+                            theme="small"
+                            label="Park"
+                            .items="${this.yesNoOptions}"
+                        ></vaadin-select>
+
+
+                        <breakdown-button ${ref(this.breakdown)} style="" label="Left"></breakdown-button>
+
+                    </div>
+                </div>
+
+
+
+                <div id="right" >
+                    <vaadin-text-area
+                        style="width:100%; height: 100%;"
+                        ${ref(this.comments)}
+                        label="Comments?"
+                    ></vaadin-text-area>
+
+
+                </div>
+            </div>
+        `;
+    }
+
+    /**
+     * Combines all the data into JSON.
+     * @returns An object containing this element's data
+     */
+    getInfo() {
+        return {
+            //TODO return data
+        };
+    }
+
+    /**
+     * Prepares this element for a new scouting session.
+     *
+     * Resets all values to their defaults.
+     */
+    reset() {
+        //TODO reset data
+    }
+
+
 }
 
 declare global {
-	interface HTMLElementTagNameMap {
-		"end-screen": EndScreen;
-	}
+    interface HTMLElementTagNameMap {
+        "end-screen": EndScreen;
+    }
 }
